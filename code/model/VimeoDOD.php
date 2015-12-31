34ffa1f93b880d86937e95a933de99b55c167932
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:31:41 +1300

    Converted to PSR-2

--- code/model/VimeoDOD.php
+++ code/model/VimeoDOD.php
@@ -1,54 +1,55 @@
 <?php
 
-class VimeoDOD extends DataExtension {
+class VimeoDOD extends DataExtension
+{
 
-	private static $has_one = array(
-		"VimeoDataObject" => "VimeoDataObject"
-	);
+    private static $has_one = array(
+        "VimeoDataObject" => "VimeoDataObject"
+    );
 
-	private static $exclude_vimeo_from_page_classes = array();
+    private static $exclude_vimeo_from_page_classes = array();
 
-	private static $include_vimeo_in_page_classes = array();
+    private static $include_vimeo_in_page_classes = array();
 
-	public function updateCMSFields(FieldList $fields) {
-		if($this->HasVimeo()) {
-			$listObject = VimeoDataObject::get();
-			if($listObject->count()) {
-				$tab = _t("VimeoDOD.TAB", "Root.Vimeo");
-				$list = array( 0 => _t("VimeoDOD.EMPTYSTRING", "--- select vimeo video ---")) + $listObject->map($index = 'ID', $titleField = 'Title')->toArray();
-				$fields->addFieldToTab($tab, new DropdownField("VimeoDataObjectID", _t("VimeoDOD.URLFIELD", "Video"), $list));
-				$linkToModelAdmin = _t("VimeoDOD.LINKTOMODELADMIN", "To edit your videos, please go to <a href=\"/admin/vimeos\">Vimeo Editing Page</a>.");
-				$fields->addFieldToTab($tab, new LiteralField("VimeoDataObjectIDEDIT", "<p>$linkToModelAdmin</p>"));
-			}
-		}
-		return $fields;
-	}
+    public function updateCMSFields(FieldList $fields)
+    {
+        if ($this->HasVimeo()) {
+            $listObject = VimeoDataObject::get();
+            if ($listObject->count()) {
+                $tab = _t("VimeoDOD.TAB", "Root.Vimeo");
+                $list = array( 0 => _t("VimeoDOD.EMPTYSTRING", "--- select vimeo video ---")) + $listObject->map($index = 'ID', $titleField = 'Title')->toArray();
+                $fields->addFieldToTab($tab, new DropdownField("VimeoDataObjectID", _t("VimeoDOD.URLFIELD", "Video"), $list));
+                $linkToModelAdmin = _t("VimeoDOD.LINKTOMODELADMIN", "To edit your videos, please go to <a href=\"/admin/vimeos\">Vimeo Editing Page</a>.");
+                $fields->addFieldToTab($tab, new LiteralField("VimeoDataObjectIDEDIT", "<p>$linkToModelAdmin</p>"));
+            }
+        }
+        return $fields;
+    }
 
-	function HasVimeo() {
-		$hasVimeo = true;
-		$includeClasses = $this->owner->Config()->get("include_vimeo_in_page_classes");
-		if(count($includeClasses)) {
-			if(!in_array($this->owner->ClassName, Config::inst()->get("VimeoDOD", "include_vimeo_in_page_classes"))) {
-				$hasVimeo = false;
-			}
-		}
-		$excludeClasses = Config::inst()->get("VimeoDOD", "exclude_vimeo_from_page_classes");
-		if(count($excludeClasses)) {
-			if(in_array($this->owner->ClassName, $excludeClasses)) {
-				$hasVimeo = false;
-			}
-		}
-		return $hasVimeo;
-	}
+    public function HasVimeo()
+    {
+        $hasVimeo = true;
+        $includeClasses = $this->owner->Config()->get("include_vimeo_in_page_classes");
+        if (count($includeClasses)) {
+            if (!in_array($this->owner->ClassName, Config::inst()->get("VimeoDOD", "include_vimeo_in_page_classes"))) {
+                $hasVimeo = false;
+            }
+        }
+        $excludeClasses = Config::inst()->get("VimeoDOD", "exclude_vimeo_from_page_classes");
+        if (count($excludeClasses)) {
+            if (in_array($this->owner->ClassName, $excludeClasses)) {
+                $hasVimeo = false;
+            }
+        }
+        return $hasVimeo;
+    }
 
-	function VimeosInThisSection(){
-		return Page::get()->filter(array(
-			"ParentID" => array(intval($this->owner->ParentID), intval($this->owner->ID)),
-			"VimeoDataObjectID:GreaterThan" => 0,
-			"ShowInSearch" => 1
-		));
-	}
-
+    public function VimeosInThisSection()
+    {
+        return Page::get()->filter(array(
+            "ParentID" => array(intval($this->owner->ParentID), intval($this->owner->ID)),
+            "VimeoDataObjectID:GreaterThan" => 0,
+            "ShowInSearch" => 1
+        ));
+    }
 }
-
-
--- code/tasks/VimeoDataObjectRedoTask.php
+++ code/tasks/VimeoDataObjectRedoTask.php
@@ -1,21 +1,22 @@
 <?php
 
-class VimeoDataObjectRedoTask extends BuildTask {
+class VimeoDataObjectRedoTask extends BuildTask
+{
 
-	protected $title = "Redo meta-data for Vimeo Objects";
+    protected $title = "Redo meta-data for Vimeo Objects";
 
-	protected $description = "Removes all the cached meta-data for all vimeo objects and re-applies them. Should end with the word Completed.";
+    protected $description = "Removes all the cached meta-data for all vimeo objects and re-applies them. Should end with the word Completed.";
 
-	function run($request) {
-		$count = VimeoDataObject::get()->count() + 1;
-		for($i = 0; $i < $count; $i++) {
-			$obj = VimeoDataObject::get()->limit(1, $i)->First();
-			if($obj) {
-				DB::alteration_message("Saving data for object with code ".$obj->VimeoCode, "created");
-				$obj->HTML(true);
-			}
-		}
-		DB::alteration_message("================ COMPLETED ====================");
-	}
-
+    public function run($request)
+    {
+        $count = VimeoDataObject::get()->count() + 1;
+        for ($i = 0; $i < $count; $i++) {
+            $obj = VimeoDataObject::get()->limit(1, $i)->First();
+            if ($obj) {
+                DB::alteration_message("Saving data for object with code ".$obj->VimeoCode, "created");
+                $obj->HTML(true);
+            }
+        }
+        DB::alteration_message("================ COMPLETED ====================");
+    }
 }
