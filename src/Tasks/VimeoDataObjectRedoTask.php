1bacecf421ea89f13cb3d4f525ab0b691a560564
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-04-15 08:16:20 +1200

    API:  upgrade to new version of Silverstripe - step: Fix build tasks for Silverstripe 6.

--- src/Tasks/VimeoDataObjectRedoTask.php
+++ src/Tasks/VimeoDataObjectRedoTask.php
@@ -5,23 +5,29 @@ declare(strict_types=1);
 namespace Sunnysideup\Vimeoembed\Tasks;
 
 use SilverStripe\Dev\BuildTask;
-use SilverStripe\ORM\DB;
+use SilverStripe\PolyExecution\PolyOutput;
 use Sunnysideup\Vimeoembed\Model\VimeoDataObject;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
 
 class VimeoDataObjectRedoTask extends BuildTask
 {
+    protected static string $commandName = 'vimeo-data-object-redo';
+
     protected string $title = 'Redo meta-data for Vimeo Objects';
 
-    protected $description = 'Removes all the cached meta-data for all vimeo objects and re-applies them. Should end with the word Completed.';
+    protected static string $description = 'Removes all the cached meta-data for all vimeo objects and re-applies them.';
 
-    public function run($request)
+    protected function execute(InputInterface $input, PolyOutput $output): int
     {
         $objects = VimeoDataObject::get();
         foreach ($objects as $obj) {
-            DB::alteration_message('Saving data for object with code ' . $obj->VimeoCode, 'created');
+            $output->writeln('Saving data for object with code ' . $obj->VimeoCode);
             $obj->HTML(true);
         }
 
-        DB::alteration_message('================ COMPLETED ====================');
+        $output->writeln('================ COMPLETED ====================');
+
+        return Command::SUCCESS;
     }
 }
